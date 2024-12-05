f2de7d6b862eb3d16a26a6353ba710a8740efbda
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-05 21:47:01 +0530

    Add health-check endpoint and error handling middleware

--- Day-02/middlewareWithExample/index.js
+++ Day-02/middlewareWithExample/index.js
@@ -3,6 +3,8 @@ const app = express()
 const PORT = 3000
 
 
+app.use(express.json())
+
 /**
  * TODO:
  * Write a middleware to calculateRequest (load that is comming on my Server)
@@ -16,8 +18,33 @@ const calculateRequest = (req, res, next) => {
 }
 
 
-app.get("/", calculateRequest, (req, res) => {
+app.get("/", calculateRequest, (_, res) => {
     res.send("Hello Ji")
+})
+
+
+app.post("/health-check", (req, res) => {
+
+    // kidneys = [1,2]
+    const kidneys = req.body.kidneys
+    const kidneyLength = kidneys.length
+
+    return res
+        .status(200)
+        .json({
+            message: `You have ${kidneyLength} kidneys !!`
+        })
+
+})
+
+
+// global catchtes
+app.use((err, req, res, next) => {
+    return res
+        .status(500)
+        .json({
+            message: `Sorry, something is up with our server`
+        })
 })
 
 
