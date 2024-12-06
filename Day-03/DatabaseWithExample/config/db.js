08ddc68aeaf7fc694d84c2cce04187c3fca35641
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-06 21:46:26 +0530

    Update MongoDB connection string to use environment variable for URI

--- Day-03/DatabaseWithExample/config/db.js
+++ Day-03/DatabaseWithExample/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://abhi:Abhi123@backend-cohort.wxwmr.mongodb.net/userApp`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/userApp`)
         console.log(`mongoDB connected Successfully !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log(`mongoDB connection Failed !! ${error}`);
