61ee211b4526f70da8d89aabf7b65c6967f6d52d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-08 01:10:48 +0530

    add multer middleware for file uploads and include cookie-parser in app setup

--- Projects/01_videoTube/src/app.js
+++ Projects/01_videoTube/src/app.js
@@ -1,23 +1,27 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 // Mddlewares
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 // common middleware
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //import routes
-import healthcheckRouter from "./routes/healthcheck.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // routes
-app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
-export { app }
+export { app };
