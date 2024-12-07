c1ba1b1a0c98352e303747a3ffbe6239b977641b
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-08 00:53:48 +0530

    add bcrypt and jsonwebtoken for password hashing and token generation in user model

--- Projects/01_videoTube/src/models/user.models.js
+++ Projects/01_videoTube/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -8,7 +10,7 @@ const userSchema = new Schema(
             unique: true,
             lowercase: true,
             trim: true,
-            index: true
+            index: true,
         },
 
         email: {
@@ -16,43 +18,80 @@ const userSchema = new Schema(
             required: true,
             unique: true,
             lowercase: true,
-            trim: true
+            trim: true,
         },
 
         fullName: {
             type: String,
             required: true,
             trim: true,
-            index: true
+            index: true,
         },
 
         avatar: {
             type: String, // cloudinary url
-            required: true
+            required: true,
         },
 
         coverImage: {
             type: String, // cloudinary url
-            required: true
+            required: true,
         },
 
         watchHistory: [
             {
                 type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
+                ref: "Video",
+            },
         ],
 
         password: {
             type: String,
-            required: [true, "password is required"]
+            required: [true, "password is required"],
         },
 
         refreshToken: {
-            type: String
-        }
+            type: String,
+        },
     },
-    {timestamps: true}
-)
+    { timestamps: true }
+);
 
-export const User = mongoose.model("User", userSchema)
+//TODO: Hash password before Saving to DB
+userSchema.pre("save", async function (next) {
+    if (!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// TODO: Compare `user password` and `saved password`: return true/false
+userSchema.methods.isPassworCorrect = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+// TODO: generateAccessToken
+userSchema.methods.generateAccessToken = function () {
+    // short lived Token
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+};
+
+// TODO: generateRefreshToken
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: REFRESH_TOKEN_EXPIRY }
+    );
+};
+export const User = mongoose.model("User", userSchema);
