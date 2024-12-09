cf1697282326f5eaf874fdea5303b2f874f31e0d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-09 15:49:20 +0530

    improve MongoDB connection log formatting for better readability

--- Projects/01_videoTube/src/db/index.js
+++ Projects/01_videoTube/src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`MongoDB connected ! DB host: ${connectionInstance.connection.host}`)
+        console.log(`\n MongoDB connected ! DB host: ${connectionInstance.connection.host}`)
         // console.log(connectionInstance)
     } catch (error) {
         console.log("MongoDB Connection error", error)
