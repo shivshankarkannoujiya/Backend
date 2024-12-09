f59bfac887eafd6d4160d53bbdd0e5d3fb7a47f1
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-09 21:55:26 +0530

    refactor user registration route

--- Projects/01_videoTube/src/routes/user.routes.js
+++ Projects/01_videoTube/src/routes/user.routes.js
@@ -1,21 +1,9 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
-import { upload } from "../middlewares/multer.middlewares.js";
+
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1,
-        },
-        {
-            name: "coverImage",
-            maxCount: 1,
-        },
-    ]),
-    registerUser
-);
+router.route("/register").post(registerUser);
 
 export default router;
