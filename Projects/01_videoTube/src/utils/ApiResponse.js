a5d3af93c57f410aac4b0122f212f7d5267fccdb
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-09 14:33:54 +0530

    refactor ApiResponse class for improved code consistency and formatting

--- Projects/01_videoTube/src/utils/ApiResponse.js
+++ Projects/01_videoTube/src/utils/ApiResponse.js
@@ -1,11 +1,10 @@
-
 class APiResponse {
     constructor(statusCode, data, message = "Success") {
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
     }
 }
 
-export { APiResponse }
+export { APiResponse };
