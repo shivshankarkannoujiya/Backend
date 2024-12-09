0fcdaa7cb7741035cb3383daf6c870c246a819e8
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-09 14:46:46 +0530

    add user routes for user-related API endpoints

--- Projects/01_videoTube/src/app.js
+++ Projects/01_videoTube/src/app.js
@@ -20,8 +20,10 @@ app.use(cookieParser());
 
 //import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
 
 export { app };
