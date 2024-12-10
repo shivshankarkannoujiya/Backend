1ea1f807df387c32507d7519285c1a2f588ae932
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-11 03:06:05 +0530

    initialize course selling application with Express, MongoDB connection, and basic structure

---
+++ Day-04/readme.md
@@ -0,0 +1,12 @@
+## Create a Course selling Application 
+
+### Description
+It need to support 2 types of Users 
+1. Admin
+2. Users
+
+Admin are allowed to signup,create courses,
+Users are allowed to signup, view courses, purchase courses
+This in the real world translate to an app like Udemy
+ 
+
---
+++ Day-04/src/index.js
@@ -0,0 +1,20 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000
+
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(`mongoDB connection ERROR: ${err}`)
+    })
