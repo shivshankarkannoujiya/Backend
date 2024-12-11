8b2163d6cb9d20d9fbb21aa6ffa9295d011d7416
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-12 01:42:18 +0530

    refactor: standardize spacing and improve error handling in admin and user signup functions

--- Day-04/src/controllers/admin.controllers.js
+++ Day-04/src/controllers/admin.controllers.js
@@ -1,43 +1,45 @@
 import { Admin } from "../models/admin.models.js";
 
 const signupAdmin = async (req, res) => {
-   const { username, password } = req.body;
+    const { username, password } = req.body;
 
-   if (!username || !password) {
-      return res.status(400).json({
-         message: "All fields are required",
-      });
-   }
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "All fields are required",
+        });
+    }
 
-   try {
-      const existedAdmin = await Admin.findOne({ username: username });
-      if (existedAdmin) {
-         return res.status(409).json({
-            message: "Admin already exist with username",
-         });
-      }
+    try {
+        const existedAdmin = await Admin.findOne({ username: username });
+        if (existedAdmin) {
+            return res.status(409).json({
+                message: "Admin already exist with username",
+            });
+        }
 
-      const admin = await Admin.create({
-         username,
-         password,
-      });
+        const admin = await Admin.create({
+            username,
+            password,
+        });
 
-      const createdAdmin = await Admin.findById(admin._id).select("-password");
-      if (!createdAdmin) {
-         return res.status(500).json({
-            message: "Something went wrong while creating admin",
-         });
-      }
+        const createdAdmin = await Admin.findById(admin._id).select(
+            "-password"
+        );
+        if (!createdAdmin) {
+            return res.status(500).json({
+                message: "Something went wrong while creating admin",
+            });
+        }
 
-      return res.status(201).json({
-         data: createdAdmin,
-         message: "Admin signup successfully",
-      });
-   } catch (error) {
-      return res.status(500).json({
-         message: "Something went wrong while registering Admin",
-      });
-   }
+        return res.status(201).json({
+            data: createdAdmin,
+            message: "Admin signup successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong while registering Admin",
+        });
+    }
 };
 
 export { signupAdmin };
--- Day-04/src/controllers/user.controllers.js
+++ Day-04/src/controllers/user.controllers.js
@@ -1,41 +1,43 @@
 import { User } from "../models/user.models.js";
 
 const signupUser = async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  if (!username || !password) {
-    return res.status(400).json({
-      message: "All fields are required",
-    });
-  }
-
-  try {
-    const existedUser = await User.findOne({ username });
-    if (existedUser) {
-      return res.status(409).json({
-        message: "User already exist with username",
-      });
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "All fields are required",
+        });
     }
 
-    const user = await User.create({
-      username,
-      password,
-    });
+    try {
+        const existedUser = await User.findOne({ username });
+        if (existedUser) {
+            return res.status(409).json({
+                message: "User already exist with username",
+            });
+        }
 
-    const createdUser = await User.findById(user._id).select("-password");
-    if (!createdUser) {
-      return res.status(500).json("Something went wrong while creating user");
+        const user = await User.create({
+            username,
+            password,
+        });
+
+        const createdUser = await User.findById(user._id).select("-password");
+        if (!createdUser) {
+            return res
+                .status(500)
+                .json("Something went wrong while creating user");
+        }
+
+        return res.status(201).json({
+            data: createdUser,
+            message: "User created successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
     }
-
-    return res.status(201).json({
-      data: createdUser,
-      message: "User created successfully",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
 };
 
 export { signupUser };
