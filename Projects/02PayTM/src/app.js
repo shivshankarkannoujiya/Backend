a9e05c848d5a889373c0568a3edf972009216ab4
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-13 04:23:52 +0530

    feat: initialize PayTM application

---
+++ Projects/02PayTM/src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ limit: "16kb", extended: true }))
+
+
+
+export { app }
