e5784b8717336061a4a4041bd776d060940fc891
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-14 07:23:29 +0530

    add transfer balance functionality with transaction handling

--- Projects/02PayTM/src/controllers/account.controllers.js
+++ Projects/02PayTM/src/controllers/account.controllers.js
@@ -1,16 +1,80 @@
-import { Account } from "../models/account.models.js"
-
+import { Account } from "../models/account.models.js";
+import mongoose from "mongoose";
 
 const getBalance = async (req, res) => {
-    const userId = req.user?._id
-    const account = await Account.findOne({ userId })
-    
-    return res
-        .status(200)
-        .json({
-            balance: account.balance
-        })
-}
+    const userId = req.user?._id;
+    const account = await Account.findOne({ userId });
 
+    return res.status(200).json({
+        balance: account.balance,
+    });
+};
 
-export { getBalance }
+const transferBalance = async (req, res) => {
+    try {
+        const session = await mongoose.startSession();
+
+        session.startTransaction();
+        const { amount, to } = req.body;
+
+        if (!amount || !to) {
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "amount and recipient account are required",
+            });
+        }
+
+        if (amount <= 0) {
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Invalid transfer amount",
+            });
+        }
+
+        // Fetch the accounts within the transaction
+        const account = await Account.findOne({
+            userId: req.user?._id,
+        }).session(session);
+
+        if (!account || account.balance < amount) {
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Insufficient balance",
+            });
+        }
+
+        const toAccount = await Account.findOne({ userId: to }).session(
+            session
+        );
+        if (!toAccount) {
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "recipient account not found",
+            });
+        }
+
+        // Perform the Transaction
+        await Account.updateOne(
+            { userId: req.user?._id },
+            { $inc: { balance: -amount } }
+        ).session(session);
+
+        await Account.updateOne(
+            { userId: to },
+            { $inc: { balance: amount } }
+        ).session(session);
+
+        // commit Transaction
+        await session.commitTransaction();
+        return res.status(200).json({
+            message: "Transfer successful",
+        });
+    } catch (error) {
+        console.log("Error during transfer : ", error);
+        return res.status(500).json({
+            message: "An error occured while processing the transfer",
+        });
+    }
+};
+
+export { getBalance, transferBalance };
--- Projects/02PayTM/src/routes/account.routes.js
+++ Projects/02PayTM/src/routes/account.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getBalance } from "../controllers/account.controllers.js";
+import {
+    getBalance,
+    transferBalance,
+} from "../controllers/account.controllers.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/balance").get(authMiddleware, getBalance);
+router.route("/transfer").post(authMiddleware, transferBalance);
 
 export default router;
