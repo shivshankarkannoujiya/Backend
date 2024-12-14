42e222fba675ca1bdb859a94d6ed8f3f5da8998d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-14 06:08:03 +0530

    feat: add account model

---
+++ Projects/02PayTM/src/models/account.models.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        balance: {
+            type: Number,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Account = mongoose.model("Account", accountSchema);
